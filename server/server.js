31b7626f4e123f29f311668f0a906cf006ca77c0
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-05-14 01:44:50 +0300

    Makale tipi oluşturuldu

--- server/server.js
+++ server/server.js
@@ -1,43 +1,51 @@
 // ApolloServer sınıfını apollo-server paketinden import ediyoruz.
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
 
 // gql, GraphQL şemasını tanımlamak için kullanılan bir tag fonksiyonudur
 const gql = require("graphql-tag");
 
-
 // GraphQL şema tanımı yapılır
 // Burada sadece bir Query tipi tanımlanıyor: ilkTip adlı bir alan var ve String (zorunlu) döner
 const typeDefs = gql`
-    type Query {
-        ilkTip: String!
-    }
+  type Makale {
+    id: ID!
+    baslik: String!
+    icerik: String!
+  }
+
+  type Query {
+    makalelerGetir: [Makale]!
+  }
 `;
 
 // Resolver fonksiyonları tanımlanır
-// ilkTip alanı çağrıldığında ne döneceğini belirtiyoruz
+// makalelerGetir alanı çağrıldığında ne döneceğini belirtiyoruz
 const resolvers = {
-    Query : {
-        // ilkTip sorgusu çalıştırıldığında bu fonksiyon çalışır
-        // Geriye bir metin döner
-        ilkTip: () => {
-            return 'ilk tip oluşturuldu';
-        }
-    }
-}
+  Query: {
+    makalelerGetir() {
+      const makaleler = [
+        { id: 1, baslik: "makale baslik 1", icerik: "makale içerik 1" },
+        { id: 2, baslik: "makale baslik 2", icerik: "makale içerik 2" },
+        { id: 3, baslik: "makale baslik 3", icerik: "makale içerik 3" },
+      ];
+
+      return makaleler;
+    },
+  },
+};
 
 // Apollo Server örneği oluşturuluyor
 // Şema (typeDefs) ve resolver'lar sunucuya verilir
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
 // Sunucu 5000 portunda dinlenmeye başlar
 // Başarıyla başlarsa, terminale sunucu adresini yazar
 server.listen({ port: 5000 }).then((res) => {
-    console.log(`server ${res.url} adresinde çalışıyor.`)
+  console.log(`server ${res.url} adresinde çalışıyor.`);
 });
-
 
 /*
 
